703e98cde71c1eed863511e5873944f29e74234d
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-08-12 15:09:01 -0500

    Adding Tab bar controller and Storyboard reference to the project

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -25,6 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().backIndicatorImage = backButtonImage
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = backButtonImage
         
+        // Customize the tab bar
+        UITabBar.appearance().tintColor = UIColor(red: 231, green: 76, blue: 60)
+        UITabBar.appearance().barTintColor = UIColor(red: 250, green: 250, blue: 250)
+        
         return true
     }
 
