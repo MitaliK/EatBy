33f262414d2fa112f029235e7e4760057e9edf1c
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-08-14 20:09:32 -0500

    - Created Discover Storyboard and About Storyboard - Created About page with WKWebView and SFSafariViewController - Added images for About Page - Enabled Cloud Kit capability - Added NSAppTransportSecurity to avoid ATS

---
+++ FoodPin/Controller/AboutTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  AboutTableViewController.swift
+//  FoodPin
+//
+//  Created by Mitali Kulkarni on 12/08/18.
+//  Copyright © 2018 Mitali Kulkarni. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class AboutTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var sectionTitles = ["Feedback", "Follow Us"]
+    var sectionContent = [[(image: "store", text: "Rate us on App Store", link: "https://www.apple.com/itunes/charts/paid-apps/"),
+    (image: "chat", text: "Tell us your feedback", link: "http://www.appcoda.com/contact")],
+    [(image: "twitter", text: "Twitter", link: "https://twitter.com/appcodamobile"),
+    (image: "facebook", text: "Facebook", link: "https://facebook.com/appcodamobile"),
+    (image: "instagram", text: "Instagram", link: "https://www.instagram.com/appcodadotcom")]]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // For iPAD
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        
+        // For Large Title Bar
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // largeTitleTextAttributes: used to customize navigation bars large title text
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            // NSAttributedStringKey is renamed to NSAttributedString.Key
+            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
+        }
+        
+        // Remove extra separators
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return sectionTitles.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return sectionContent[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AboutCell", for: indexPath)
+        
+        // Configure the cell...
+        let cellData = sectionContent[indexPath.section][indexPath.row]
+        cell.textLabel?.text = cellData.text
+        cell.imageView?.image = UIImage(named: cellData.image)
+        
+        return cell
+    }
+    
+    // MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let link = sectionContent[indexPath.section][indexPath.row].link
+        
+        switch indexPath.section {
+            // Leave us feedback section
+        case 0:
+            if indexPath.row == 0 {
+                if let url = URL(string: link) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            } else if indexPath.row == 1 {
+                performSegue(withIdentifier: "showWebView", sender: self)
+            }
+            // Follow us section
+        case 1:
+            if let url = URL(string: link) {
+                let safariController = SFSafariViewController(url: url)
+                present(safariController, animated: true, completion: nil)
+            }
+        default:
+            break
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "showWebView" {
+            if let destinationController = segue.destination as? WebViewController, let indexPath = tableView.indexPathForSelectedRow {
+                destinationController.target = sectionContent[indexPath.section][indexPath.row].link
+            }
+        }
+    }
+}
