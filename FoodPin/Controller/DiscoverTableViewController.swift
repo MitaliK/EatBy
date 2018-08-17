49102908a0f90b4ff82154e973802f1b733d48c6
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-08-17 10:50:44 -0500

    - Fetch data from iCloud using Convenience and Operational API. - Added Spinner in discover tab - display the data retrieved from iCloud in the table view

--- FoodPin/Controller/DiscoverTableViewController.swift
+++ FoodPin/Controller/DiscoverTableViewController.swift
@@ -7,84 +7,135 @@
 //
 
 import UIKit
+import CloudKit
 
 class DiscoverTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    var restaurants: [CKRecord] = []
+    var spinner = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // For iPAD
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        
+        // For Large Title Bar
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // largeTitleTextAttributes: used to customize navigation bars large title text
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            // NSAttributedStringKey is renamed to NSAttributedString.Key
+            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
+        }
+        
+        // When the Discover tab is loaded, the app will start to fetch the records via CloudKit.
+        fetchRecordsFromCloud()
+        
+        // Adding spinner
+        spinner.activityIndicatorViewStyle = .gray
+        spinner.hidesWhenStopped = true
+        view.addSubview(spinner)
+        
+        // Define layout constraints on the spinner view
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150.0), spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+        
+        // Activate the spinner
+        spinner.startAnimating()
+    }
+    
+    // MARK: - Helper Function
+    /*func fetchRecordsFromCloud() {
+        
+        // Fetching data using Convenience API
+        let cloudContainer = CKContainer.default()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        
+        // Convenience API
+        publicDatabase.perform(query, inZoneWith: nil) { (result, error) in
+            
+            if let error = error {
+                print("Error in fetching data from Cloud: \(error)")
+                return
+            }
+            
+            if let results = result {
+                print("Completed the download of data from Cloud")
+                self.restaurants = results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    } */
+    
+    func fetchRecordsFromCloud() {
+        
+        // Fetching data using Convenience API
+        let cloudContainer = CKContainer.default()
+        // Get public database
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        
+        // create query
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        
+        // Create query operation with the query
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.desiredKeys = ["name","image"]
+        queryOperation.queuePriority = .veryHigh
+        queryOperation.resultsLimit = 50
+        queryOperation.recordFetchedBlock = {(record) -> Void in
+            self.restaurants.append(record)
+        }
+        
+        queryOperation.queryCompletionBlock = {(cursor, error) -> Void in
+            if let error = error {
+                print("Failed to get data from iCloud : \(error.localizedDescription)")
+                return
+            }
+            print("Successfully retreived data from iCloud")
+            DispatchQueue.main.async {
+                self.spinner.stopAnimating()
+                self.tableView.reloadData()
+            }
+        }
+        
+        // Execute query
+        publicDatabase.add(queryOperation)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.restaurants.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
         // Configure the cell...
-
+        let restaurant = self.restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        
+        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
+            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL) {
+                cell.imageView?.image = UIImage(data: imageData)
+            }
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
